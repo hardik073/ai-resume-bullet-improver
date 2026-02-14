387c1a530da1f4789897384bd073010d37c93251
Hardik073 <vegadhardikr@gmail.com>
2026-02-14 18:21:17 +0530

    Integrated OpenAI API

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
+
 def improve_resume_bullet(bullet):
-    #We will connect AI here
-    return "Improved version will appear here"
+    prompt = f"""
+    Rewrite the following resume bullet to make it more impactful,
+    professional, and achievement-oriented:
+
+    {bullet}
+    """
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role":"user","content":prompt}
+        ],
+        temperature=0.7
+    )
+
+    return response.choices[0].message.content.strip()
 
 def main():
     print("=== AI Resume Bullet Improver ===")
