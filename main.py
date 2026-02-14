1a13ee1e06cea130a1ffb61cfc229cb72db3511f
Hardik073 <vegadhardikr@gmail.com>
2026-02-14 10:56:07 +0530

    Initial CLI Structure for resume improver

---
+++ main.py
@@ -0,0 +1,14 @@
+def improve_resume_bullet(bullet):
+    #We will connect AI here
+    return "Improved version will appear here"
+
+def main():
+    print("=== AI Resume Bullet Improver ===")
+    user_input = input("Enter your resume bullet:\n> ")
+    improved = improve_resume_bullet(user_input)
+
+    print("\nImproved Version:")
+    print(improved)
+
+if __name__ == "__main__":
+    main()
